8b8dcae57ee72b9d8349c26d1a28cbfe529c3cc0
1883456123 <150493727+1883456123@users.noreply.github.com>
2024-07-04 17:23:19 +0800

    worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '6b301f7f-83e5-47b9-b68b-7cdac06e00c7';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
